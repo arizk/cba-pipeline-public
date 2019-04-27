2b9364a39d8473c5ab07166aaef8aa915ec1fc4d
Trevor Ballard <trevor.ballard@outlook.com>
2019-04-27 19:50:07 -0400

    Clean up start.sh

--- containernet/ndn-experiment/start.sh
+++ containernet/ndn-experiment/start.sh
@@ -1,56 +1,41 @@
 #!/bin/bash
 
-#TODO Pull ndn_repo and ndn_headless-player folders from somewhere
-
-copyres () {
-  docker cp mn.client1:/log /home/ubuntu/containernet/results/client1/$(date +%Y%m%d%T)_log; docker cp mn.client2:/log /home/ubuntu/containernet/results/client2/$(date +%Y%m%d%T)_log;
-  docker cp mn.client1:/log_ChunktimeObserver /home/ubuntu/containernet/results/client1/$(date +%Y%m%d%T)_log_ChunktimeObserver; docker cp mn.client2:/log_ChunktimeObserver /home/ubuntu/containernet/results/client2/$(date +%Y%m%d%T)_log_ChunktimeObserver;
-}
-export -f copyres
-
-cleandocker ()
-{
-  docker rm $(docker ps -a -q)
-  docker rmi --force $(docker images -q)
-  docker system prune --force
-
-  # Double-tap
-  systemctl stop docker
-  rm -rf /var/lib/docker/aufs
-  apt-get autoclean
-  apt-get autoremove
-  systemctl start docker
-}
-export -f cleandocker
-
-# Make sure the last run has been obtained
-if [ ! -z "$(ls -A /home/ubuntu/containernet/results/)" ]; then
-   echo "../results directory not empty. Store the results somewhere, then do 'rm ../results/*'. Aborting."
-   exit 1
-fi
-
-if [ "$1" == '--build-docker' ]
-then
-    echo "Build Containers"
-    #docker rmi $(docker images -q)
-    cd /home/ubuntu/containernet/ndn-containers/ndn_headless-player/
-    docker-compose build ndn-player
-    cd /home/ubuntu/containernet/ndn-containers/ndn_repo/
-    docker-compose build --build-arg STORAGE_TYPE=repong-bbb ndn
-fi
-if [ "$1" == '--build-repo' ]
-then
-    echo "Build Repo"
-    cd /home/ubuntu/containernet/ndn-containers/ndn_repo/
-    docker-compose build --build-arg STORAGE_TYPE=repong-bbb ndn
-fi
-if [ "$1" == '--build-player' ]
-then
+# Useful functions
+buildPlayer() {
     echo "Build Player"
     cd /home/ubuntu/containernet/ndn-containers/ndn_headless-player/
     docker-compose build ndn-player
+}
+
+buildRepo() {
+	echo "Build Repo"
+	cd /home/ubuntu/containernet/ndn-containers/ndn_repo/
+	docker-compose build --build-arg STORAGE_TYPE=repong-bbb ndn
+}
+
+# Ensure results folder exists and is empty
+if [ ! -d "/home/ubuntu/containernet/results/" ]; then
+	mkdir /home/ubuntu/containernet/results/
 fi
-echo "Start Test"
+if [ ! -z "$(ls -A /home/ubuntu/containernet/results/)" ]; then
+	echo "../results directory not empty. Store the results somewhere, then do 'rm ../results/*'. Aborting."
+	exit 1
+fi
+
+# Build containers if desired
+if [ "$1" == '--build-docker' ]; then
+	buildPlayer
+	buildRepo
+fi
+if [ "$1" == '--build-repo' ]; then
+   buildRepo
+fi
+if [ "$1" == '--build-player' ]; then
+	buildPlayer
+fi
+
+# Begin the evaluation
+echo "Beginning evaluation"
 cd /home/ubuntu/containernet/ndn-experiment
 
 # Clear out old ucb objects
@@ -66,7 +51,6 @@ for epoch in $(seq 1 5); do
     for qoe_weights in "${qoe_weights_list[@]}"; do
         mkdir -p /home/ubuntu/containernet/ucb_objs/client{1..10}/${qoe_weights// /_}
         for algo in "${algo_list[@]}"; do
-			#echo ""
             # If it's a bandit, try it with memory. Either way, also do it without.
             if [[ "$algo" =~ ^(sbu|sbuose|linucb)$ ]]; then
                 docker stop $(docker ps -aq)
